85f2bea348d3148eab5f7aa43c570ca2021a9ad5
Kristian <49764796+kristianka@users.noreply.github.com>
2026-02-13 01:02:49 +0200

    fix capitalisation

--- frontend/src/components/ui/carda.tsx
+++ frontend/src/components/ui/carda.tsx
@@ -66,6 +66,7 @@ function CardAction({ className, ...props }: React.ComponentProps<"div">) {
   );
 }
 
+//
 function CardContent({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
